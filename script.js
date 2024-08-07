28b0311bc30b46cdabb8fdc87619fb13c4e1107f
joao-sillva <joao.vgsillva@gmail.com>
2024-08-07 20:37:25 -0300

    Formater currency total

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const footer = document.querySelector('main footer')
 const amount = document.getElementById('amount')
 const currency = document.getElementById('currency')
 const description = document.getElementById('description')
+const result = document.getElementById('result')
 
 amount.addEventListener('input', () => {
   const hasCaractersRegex = /\D+/g
@@ -36,11 +37,27 @@ form.onsubmit = (event) => {
 
 function convertCurrency(amount, price, symbol) {
   try {
-    description.textContent = `${symbol} 1 = ${price}`
-    
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    let total = amount * price
+
+    if (isNaN(total)) {
+      return alert('Por favor, digite o valor corretamente para converter!')
+    }
+
+    total = formatCurrencyBRL(total).replace('R$', '')
+    result.textContent = `${total} Reais`
+
     footer.classList.add('show-result')
   } catch (error) {
     footer.classList.remove('show-result')
     alert('Não foi possível converter. Tente novamente!')
   }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  })
 }
