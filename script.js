917025f8eaeb7e6d27b968f804c65e3f8ba827e6
joao-sillva <joao.vgsillva@gmail.com>
2024-08-07 03:50:45 -0300

    Get selected currency

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const currency = document.getElementById('currency')
 
 amount.addEventListener('input', () => {
   const hasCaractersRegex = /\D+/g
   amount.value = amount.value.replace(hasCaractersRegex, '')
 })
+
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case 'USD':
+      convertCurrency(amount.value, USD, 'US$')
+      break
+
+    case 'EUR':
+      convertCurrency(amount.value, EUR, '€')
+      break
+
+    case 'GBP':
+      convertCurrency(amount.value, GBP, '£')
+      break
+
+    default:
+      break
+  }
+}
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
